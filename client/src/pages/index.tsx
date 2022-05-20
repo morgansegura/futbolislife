cd40bce5f304a92ebef50c9673e7a2455d6c088c
Morgan Segura <morgan@jointhebibleproject.com>
2022-05-20 09:22:24 -0700

    FTB-1 Updates directories and cleans out scaffold styles

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Layout, { siteTitle } from 'components/layout'
+import { getSortedPostsData } from 'lib/posts'
 import Link from 'next/link'
-import Date from '../components/date'
+import Date from 'components/date'
 import { GetStaticProps } from 'next'
 
 export default function Home({
@@ -20,23 +19,23 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section className=''>
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
+      <section className=''>
+        <h2 className=''>Blog</h2>
+        <ul className=''>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
+            <li className='' key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
-              <small className={utilStyles.lightText}>
+              <small className=''>
                 <Date dateString={date} />
               </small>
             </li>
